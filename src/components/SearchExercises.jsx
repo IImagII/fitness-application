95e5cb65481062671273d0fb7ff2fb77f0ddc05a
iimagii <mag_1@ukr.net>
2023-03-17 12:13:58 +0200

    делаю горизонтальный скрол бар

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,122 @@
-import React from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { fetchData, exerciseOptions } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
-const SearchExercises = () => {
-   return <div>SearchExercises</div>
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+   const [search, setSearch] = useState('') //состояние для управляемого input (TextField)
+
+   const [bodyParts, setBodyParts] = useState([]) // тут храниться все что пришло от API
+
+   //будет при первой загрузке
+   useEffect(() => {
+      const fetchExercisesData = async () => {
+         const bodyPartsData = await fetchData(
+            'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+            exerciseOptions
+         )
+
+         setBodyParts(['all', ...bodyPartsData])
+      }
+      fetchExercisesData()
+   }, [])
+
+   //функция по реализации поиска
+   const handleSearch = async () => {
+      if (search) {
+         const exercisesData = await fetchData(
+            'https://exercisedb.p.rapidapi.com/exercises',
+            exerciseOptions
+         )
+
+         //реализация самого поиска
+         const searchedExercises = exercisesData.filter(
+            exercise =>
+               exercise.name.toLowerCase().includes(search) ||
+               exercise.target.toLowerCase().includes(search) ||
+               exercise.equipment.toLowerCase().includes(search) ||
+               exercise.bodyPart.toLowerCase().includes(search)
+         )
+         //очищаем строку поиска
+         setSearch('')
+
+         //сюда помещаем что придет после поиска
+         setExercises(searchedExercises)
+      }
+   }
+
+   return (
+      <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+         <Typography
+            fontWeight={700}
+            sx={{
+               fontSize: {
+                  lg: '44px',
+                  xs: '30px',
+               },
+            }}
+            mb='50px'
+            textAlign='center'
+         >
+            Awesome Exercises You <br />
+            Should Know
+         </Typography>
+
+         <Box position='relative' mb='72px'>
+            {/* TextField - это равносильно использованию input */}
+            <TextField
+               height='76px'
+               value={search}
+               onChange={e => setSearch(e.target.value.toLowerCase())}
+               placeholder='Search Exercises'
+               type='text'
+               sx={{
+                  input: {
+                     fontWeight: '700',
+                     border: 'none',
+                     borderRadius: '4px',
+                  },
+                  width: {
+                     lg: '800px',
+                     xs: '350px',
+                  },
+                  backgroundColor: '#fff',
+                  borderRadius: '40px',
+               }}
+            />
+
+            <Button
+               className='search-btn'
+               sx={{
+                  bgcolor: '#ff2625',
+                  color: '#fff',
+                  textTransform: 'none',
+                  width: {
+                     lg: '175px',
+                     xs: '100px',
+                  },
+                  fontSize: {
+                     lg: '24px',
+                     xs: '16px',
+                  },
+                  height: '56px',
+                  position: 'absolute',
+                  right: '0',
+               }}
+               onClick={handleSearch}
+            >
+               Search
+            </Button>
+         </Box>
+         <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+            <HorizontalScrollbar
+               data={bodyParts}
+               bodyPart={bodyPart}
+               setBodyPart={setBodyPart}
+            />
+         </Box>
+      </Stack>
+   )
 }
 
 export default SearchExercises
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import HeroBanner from '../components/HeroBanner'
 import SearchExercises from '../components/SearchExercises'
 import Exercises from '../components/Exercises'
 
 const Home = () => {
+   const [exercises, setExercises] = useState([]) // состояние куда мы помещаем даннеы после поиска
+   const [bodyPart, setBodyPart] = useState('all') // состяоние которое хранит выбор управжнения
+
    return (
       <Box>
          <HeroBanner />
-         <SearchExercises />
-         <Exercises />
+         <SearchExercises
+            setExercises={setExercises}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart}
+         />
+         <Exercises
+            setExercises={setExercises}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart}
+         />
       </Box>
    )
 }
