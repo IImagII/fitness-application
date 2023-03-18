df945a25aad967a4d62b1c989b5635999d94f023
iimagii <mag_1@ukr.net>
2023-03-18 16:50:00 +0200

    делаею компонент детализации

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 import { Box } from '@mui/material'
@@ -8,22 +8,29 @@ import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
    const { id } = useParams()
+   const [exerciseDetail, setExerciseDetail] = useState({})
 
    //функция по получению конкретного упражнения при клике на него
    useEffect(() => {
       const fetchExercisesData = async () => {
-         const bodyPartsData = await fetchData(
-            `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`,
+         //формируем url запросов у нас их будет несколько
+         const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+         const youtubeSearchUrl =
+            'https://youtube-search-and-download.p.rapidapi.com'
+
+         //тут уже жедаем запрос на само получение данных
+         const exerciseDetailData = await fetchData(
+            `${exerciseDbUrl}/exercises/${id}`,
             exerciseOptions
          )
-         console.log('bodyPartsData :>> ', bodyPartsData)
+         setExerciseDetail(exerciseDetailData)
       }
       fetchExercisesData()
    }, [id])
 
    return (
       <Box>
-         <Detail />
+         <Detail exerciseDetail={exerciseDetail} />
          <ExerciseVideos />
          <SimilarExercises />
       </Box>
