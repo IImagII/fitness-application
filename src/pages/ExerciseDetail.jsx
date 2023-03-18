addf2ad857135902ad1bad30830f90c20adeb7c1
iimagii <mag_1@ukr.net>
2023-03-18 19:09:49 +0200

    реализую компонент по выдачи видео по упражнениям

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { exerciseOptions, fetchData } from '../utils/fetchData'
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData'
 import { Box } from '@mui/material'
 import Detail from '../components/Detail'
 import ExerciseVideos from '../components/ExerciseVideos'
@@ -8,22 +8,32 @@ import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
    const { id } = useParams()
-   const [exerciseDetail, setExerciseDetail] = useState({})
+   const [exerciseDetail, setExerciseDetail] = useState({}) // состояние которое содержит упражнения из первого запроса на API
+   const [exerciseVideos, setExerciseVideos] = useState([]) // тут содержиться состояние из второго видео запроса на API
 
    //функция по получению конкретного упражнения при клике на него
    useEffect(() => {
       const fetchExercisesData = async () => {
-         //формируем url запросов у нас их будет несколько
+         /*===================================*/
+
+         //формируем url запросов у нас их будет несколько это первые запрос
          const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
          const youtubeSearchUrl =
             'https://youtube-search-and-download.p.rapidapi.com'
 
          //тут уже жедаем запрос на само получение данных
          const exerciseDetailData = await fetchData(
-            `${exerciseDbUrl}/exercises/${id}`,
+            `${exerciseDbUrl}/exercises/exercise/${id}`,
             exerciseOptions
          )
          setExerciseDetail(exerciseDetailData)
+
+         //формируем функцию для другго запроса на видео APi
+         const fetchYoutubeData = await fetchData(
+            `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+            youtubeOptions
+         )
+         setExerciseVideos(fetchYoutubeData)
       }
       fetchExercisesData()
    }, [id])
@@ -31,7 +41,10 @@ const ExerciseDetail = () => {
    return (
       <Box>
          <Detail exerciseDetail={exerciseDetail} />
-         <ExerciseVideos />
+         <ExerciseVideos
+            exerciseVideos={exerciseVideos}
+            name={exerciseDetail.name}
+         />
          <SimilarExercises />
       </Box>
    )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,20 @@
+//параметры для обычного запроса на API
 export const exerciseOptions = {
    method: 'GET',
    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
    headers: {
       'X-RapidAPI-Key': import.meta.env.VITE_SOME_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+   },
+}
+
+//параметры для видеозапроса на youtube API
+export const youtubeOptions = {
+   method: 'GET',
+
+   headers: {
+      'X-RapidAPI-Key': import.meta.env.VITE_SOME_KEY,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
    },
 }
 
