1b85ef8163ccb136aa588594dde3ec53926fa998
iimagii <mag_1@ukr.net>
2023-03-18 12:45:18 +0200

    реализуем отдельно карточки упражнений

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,33 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import ExerciseCard from './ExerciseCard'
 
-const Exercises = () => {
-   return <div>Exercises</div>
+const Exercises = ({ exercises, bodyPart, setExercises }) => {
+   console.log('🚀 ~ exercises:', exercises)
+   return (
+      <Box
+         id='exercises'
+         sx={{
+            mt: { lg: '110px' },
+         }}
+         mt='50px'
+         p='20px'
+      >
+         <Typography variant='h3' mb='46px'>
+            Showing Results
+         </Typography>
+         <Stack
+            direction='row'
+            sx={{ gap: { lg: '110px', xs: '50px' } }}
+            flexWrap='wrap'
+            justifyContent='center'
+         >
+            {exercises.map(exercise => (
+               <ExerciseCard exercise={exercise} />
+            ))}
+         </Stack>
+      </Box>
+   )
 }
 
 export default Exercises
