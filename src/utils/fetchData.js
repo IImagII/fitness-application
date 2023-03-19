8e051370f0f79a2a559df8b89288d250763319fb
iimagii <mag_1@ukr.net>
2023-03-19 15:50:25 +0200

    для deploy

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ export const exerciseOptions = {
    method: 'GET',
    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
    headers: {
-      'X-RapidAPI-Key': import.meta.env.VITE_SOME_KEY,
+      'X-RapidAPI-Key': 'bcea07917dmshb04883a2ca69c22p1c6b77jsnd696834b9264',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
    },
 }
@@ -13,7 +13,7 @@ export const youtubeOptions = {
    method: 'GET',
 
    headers: {
-      'X-RapidAPI-Key': import.meta.env.VITE_SOME_KEY,
+      'X-RapidAPI-Key': 'bcea07917dmshb04883a2ca69c22p1c6b77jsnd696834b9264',
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
    },
 }
