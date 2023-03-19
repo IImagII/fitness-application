6ba23cb21c63d852cc6d0e56ed399cfdf8a74610
iimagii <mag_1@ukr.net>
2023-03-19 15:47:40 +0200

    закончил приложение

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
-   return <div>Footer</div>
+   return (
+      <Box mt='40px' bgcolor='#fff3f4'>
+         <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+            <img src={Logo} alt='logo-footer' width='200px' height='40px' />
+            <Typography variant='h5' pb='15px' mt='5px'>
+               Made with ❤️ by JavaScript Mastery
+            </Typography>
+         </Stack>
+      </Box>
+   )
 }
 
 export default Footer
